b7fba4efd80ec1b35192c0a98f09e89419b69301
Pavel Arlou <arloupav@gmail.com>
2024-10-09 15:07:38 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import {readdir} from 'fs/promises';
+import {getPath} from "./utils/get-path.js";
+
+
+const pathForReadFiles = getPath(import.meta.url, 'files');
+
 const list = async () => {
-    // Write your code here 
+    try {
+         const fileNames= await readdir(pathForReadFiles, {recursive: true});
+        console.log(fileNames)
+    } catch (err) {
+    throw new Error('FS operation failed');
+    }
 };
 
 await list();
